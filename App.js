2770866a56aa763a209c20e1a88ee5e1f97f0dc9
ponochovny <ponochovny@gmail.com>
2020-07-19 18:40:03 +0300

    Add list and ScrollView

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Button,
+	ScrollView,
+} from 'react-native';
 
 export default function App() {
+	const [enteredGoal, setEnteredGoal] = useState('');
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		// console.log(enteredGoal);
+		setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+	};
+
 	return (
 		<View style={styles.screen}>
 			<View style={styles.inputContainer}>
-				<TextInput placeholder="Course Goal" style={styles.input} />
-				<Button title="ADD" />
+				<TextInput
+					placeholder="Course Goal"
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
+				/>
+				<Button title="ADD" onPress={addGoalHandler} />
 			</View>
-			<View />
+			<ScrollView>
+				{courseGoals.map((goal) => (
+					<View key={goal} style={styles.listItem}>
+						<Text>{goal}</Text>
+					</View>
+				))}
+			</ScrollView>
 		</View>
 	);
 }
@@ -27,5 +57,12 @@ const styles = StyleSheet.create({
 		borderWidth: 1,
 		padding: 10,
 		width: '80%',
+	},
+	listItem: {
+		padding: 10,
+		marginVertical: 10,
+		backgroundColor: '#ccc',
+		borderColor: 'black',
+		borderWidth: 1,
 	},
 });
