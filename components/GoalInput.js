b4eb3a7f993918725cdba40d4e0fdf3f73cdc889
ponochovny <ponochovny@gmail.com>
2020-07-19 19:50:51 +0300

    Styles for Modal buttons

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -22,8 +22,18 @@ const GoalInput = (props) => {
 					onChangeText={goalInputHandler}
 					value={enteredGoal}
 				/>
-				<Button title="CANCEL" color="red" onPress={props.onCancel} />
-				<Button title="ADD" onPress={addGoalHandler} />
+				<View style={styles.buttonContainer}>
+					<View style={styles.button}>
+						<Button
+							title="CANCEL"
+							color="red"
+							onPress={props.onCancel}
+						/>
+					</View>
+					<View style={styles.button}>
+						<Button title="ADD" onPress={addGoalHandler} />
+					</View>
+				</View>
 			</View>
 		</Modal>
 	);
@@ -41,6 +51,14 @@ const styles = StyleSheet.create({
 		padding: 10,
 		width: '80%',
 		marginBottom: 10,
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		width: '60%',
+	},
+	button: {
+		width: '40%',
 	},
 });
 
